0dadaa1c03b46bb56c4c32b1020081a45aa32b1b
Rahul sehrawat <rahulsehrawat1205@gmail.com>
2024-08-03 14:22:39 +0530

    custom hook UseLocalStorage added

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,33 @@ import UseRefExample from "./Hooks/UseRefExample"
 import Home from "./components/Home"
 import Foot from "./components/Foot"
 import UseReducer from "./Hooks/UseReducer"
+import UseLocalStorage from "./Hooks/UseLocalStorage"
 
 
 function App() {
+    //for custom hook local storage
+    const [name,setname] = UseLocalStorage('nameCustomHook','')
+
+
   return(
     <div className="container">
       {/* <UseStateExample/> */}
       {/* <UseEffectExample/> */}
       {/* <UseRefExample/> */}
 
+      {/* this is for UseContext */}
       {/* <Home/> */}
       {/* <Foot/> */}
+      
+      {/* <UseReducer/> */}
 
-      <UseReducer/>
-
-
+      <input 
+          type="text" 
+          placeholder="Enter your name"
+          value={name}
+          onChange={(e)=>{setname(e.target.value)}}
+          />
+      <h1> Hello,{name}</h1>
 
 
     </div>
